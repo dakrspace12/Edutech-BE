1b880240b3c30675dec0bce0e4899db0a4eff30f
ANAND KUMAR <anand9018@hotmail.com>
2024-12-02 16:21:34 +0530

    Added id and name

--- src/main/java/edutech/backend/entity/User.java
+++ src/main/java/edutech/backend/entity/User.java
@@ -1,6 +1,8 @@
 package edutech.backend.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 
@@ -8,9 +10,28 @@ import jakarta.persistence.Id;
 public class User {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
     private String email;
     private String password;
     private transient String confirmPassword;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
     public String getEmail() {
         return email;
--- src/main/java/edutech/backend/repository/UserRepository.java
+++ src/main/java/edutech/backend/repository/UserRepository.java
@@ -4,7 +4,7 @@ import edutech.backend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Integer> {
 
     // Custom query to find a user by email
     Optional<User> findByEmail(String email);
