857e24ca291b9aebc364fa70a26ab73c98b97191
Abhisek Samal <137040999+AbhisekDev72@users.noreply.github.com>
2024-12-12 16:17:25 +0530

    added deleted mapping api for courses

--- src/main/java/edutech/backend/controller/CourseController.java
+++ src/main/java/edutech/backend/controller/CourseController.java
@@ -19,14 +19,14 @@ public class CourseController {
 
     @GetMapping
     public ResponseEntity<List<Course>> getAllCourses() {
-        List<Course> courses = courseService.getAllCourses();
+        List<Course> courses = courseService.getAllCourses();  //calls all courses
         return ResponseEntity.ok(courses);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Course> getCourseById(@PathVariable Long id) {
         try {
-            Course course = courseService.findCourseById(id); // Calls the newly added method
+            Course course = courseService.findCourseById(id); // Calls the method by their ID
             return ResponseEntity.ok(course);
         } catch (CourseNotFoundException e) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null); // Handle the exception
@@ -35,8 +35,21 @@ public class CourseController {
 
     @PostMapping
     @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<Course> createCourse(@RequestBody Course course) {
+    public ResponseEntity<String> createCourse(@RequestBody Course course) {
         Course savedCourse = courseService.createCourse(course);
-        return ResponseEntity.status(HttpStatus.CREATED).body(savedCourse); // 201 Created
+        return ResponseEntity.status(HttpStatus.CREATED).body("Course: "+savedCourse+" added successfully"); // 201 Created
+    }
+
+    @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<String>deleteCourse(@PathVariable Long id){
+        try {
+            courseService.deleteCourseById(id);
+            return ResponseEntity.status(HttpStatus.CREATED).body("Course with ID "+id+" Deleted successfully");
+        }catch (CourseNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.OK).body(e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An Unexpected error occurred.");
+        }
     }
 }
